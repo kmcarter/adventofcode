531fea370ba55118ac4ac2c3f579325376544ebf
Kelly Carter <kmc4637@gmail.com>
2015-12-10 23:09:33 -0800

    Day 1: part 2 complete (answer: 1771)

--- day01/day01.rb
+++ day01/day01.rb
@@ -1,3 +1,11 @@
+#part 1
 input = File.open('input.txt').read
-floor = input.count('(') - input.count(')')
-p floor
+p input.count('(') - input.count(')')
+
+#part 2
+floor = 0
+index = input.chars.find_index do |c|
+  floor += c == '(' ? 1 : -1
+  floor < 0
+end
+p index + 1 #Santa's floor index is 1-based
