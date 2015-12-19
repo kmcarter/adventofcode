6dcaa7ab13b7e92efcc09ed2906cd4a78183df44
Kelly Carter <kmc4637@gmail.com>
2015-12-18 21:10:01 -0800

    Day 3: part 2 complete (answer: 2639)

--- day03/day03.rb
+++ day03/day03.rb
@@ -1,11 +1,19 @@
-#part 1
 input = File.open('input.txt').read
-map = { "[0, 0]" => 1 }
-current_position = [0, 0]
-input.chars.each do |c|
+map = { "[0, 0]" => 2 }
+santa_current_position = [0, 0]
+robo_current_position = [0, 0]
+input.chars.each_with_index do |c,i|
+  current_position = i % 2 == 0 ? robo_current_position : santa_current_position
   x = current_position.first + ((1 if c == '>') || (-1 if c == '<') || 0)
   y = current_position.last + ((1 if c == '^') || (-1 if c == 'v') || 0)
   current_position = [x, y]
+  
+  if i % 2 == 0
+    robo_current_position = current_position
+  else
+    santa_current_position = current_position
+  end
+  
   map[current_position.to_s] = map.fetch(current_position.to_s, 0) + 1
 end
 p map.count
