f371e74eedb534e1c3e2143b1efe41de6aaf788f
Kelly Carter <kelly@rainworksweb.com>
2018-12-03 15:46:07 -0800

    2018 day 1 part 2 completed

--- 2018/day01/day01.rb
+++ 2018/day01/day01.rb
@@ -4,4 +4,30 @@ frequency = 0
 for i in input do
     frequency += i.to_i
 end
-p "Frequency = #{frequency}"
+p "(part 1) Frequency = #{frequency}"
+
+#part 2
+#input = ['-6','+3','+8','+5','-6']
+frequency = 0
+i = 0
+already_seen_freqs = []
+freq_found_twice = false
+
+#this will run for several minutes before it finds a match
+while !freq_found_twice
+    frequency += input[i].to_i
+
+    #p already_seen_freqs.length
+    if already_seen_freqs.include?(frequency)
+        p "(part 2) First frequency to appear twice = #{frequency}"
+        freq_found_twice = true
+    else
+        already_seen_freqs << frequency
+        #p "Added #{frequency} to array"
+    end
+
+    i += 1
+    if input.length.equal?(i)
+        i = 0
+    end
+end
