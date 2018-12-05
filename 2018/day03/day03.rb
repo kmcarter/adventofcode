ba0b9a41de678d435510dd928e661247dd707180
Kelly Carter <kelly@rainworksweb.com>
2018-12-04 16:37:26 -0800

    2018 day 3 part 2 completed

--- 2018/day03/day03.rb
+++ 2018/day03/day03.rb
@@ -1,13 +1,9 @@
 #part 1
 input = File.open("input.txt").read.split("\n")
-input.map! do |row| 
-    index_of_at = row.index("@ ") + 2
-    row.slice(index_of_at..-1).split(": ")
-end
-input.map! { |x,y| [x.split(","), y.split("x")] }
+input.map! { |row| row.split(" ") }
+input.map! { |a,b,c,d| [c.delete(":").split(","), d.split("x"), a.delete("#")] }
 
 fabric_map = []
-
 input.each do |claim|
     start_vert_index = claim[0][1].to_i
     start_horz_index = claim[0][0].to_i
@@ -43,3 +39,52 @@ fabric_map.each do |row|
     end
 end
 p num_overlapping_squares
+
+#part 2
+claim_map = []
+input.each do |claim|
+    start_vert_index = claim[0][1].to_i
+    start_horz_index = claim[0][0].to_i
+    height = claim[1][1].to_i
+    width = claim[1][0].to_i
+
+    (0..height-1).each do |h|
+        v_index = start_vert_index + h
+        if claim_map[v_index] == nil
+            claim_map[v_index] = []
+        end
+
+        (0..width-1).each do |w|
+            h_index = start_horz_index + w
+            if claim_map[v_index][h_index] == nil
+                claim_map[v_index][h_index] = claim[2]
+            elsif
+                claim_map[v_index][h_index] += ",#{claim[2]}"
+            end
+        end
+    end
+end
+
+input.each do |claim|
+    start_vert_index = claim[0][1].to_i
+    start_horz_index = claim[0][0].to_i
+    height = claim[1][1].to_i
+    width = claim[1][0].to_i
+
+    skip_claim = false
+    (0..height-1).each do |h|
+        v_index = start_vert_index + h
+
+        (0..width-1).each do |w|
+            h_index = start_horz_index + w
+            if claim_map[v_index][h_index].index(",") != nil
+                skip_claim = true
+                break
+            end
+        end
+
+        break if skip_claim
+    end
+    
+    p "Magic claim: #{claim[2]}" if !skip_claim
+end
