ebc3b4b4985675e6f80cba3c2050cb0f50f5a09f
Kelly Carter <kelly@rainworksweb.com>
2018-12-04 16:47:10 -0800

    2018 day 2 part 2 completed!

--- 2018/day02/day02.rb
+++ 2018/day02/day02.rb
@@ -17,3 +17,16 @@ input.each do |ele|
     end
 end
 p "Checksum of #{num_twos} * #{num_threes} = #{num_twos * num_threes}"
+
+#part 2
+input.each do |first|
+    first_char_arr = first.chars
+    input.each do |second|
+        second_char_arr = second.chars.to_a
+
+        different_chars = 0
+        first_char_arr.each_with_index { |c,i| different_chars += 1 if c != second_char_arr[i] }
+
+        p "Box IDs #{first} and #{second} differ by 1 character!" if different_chars == 1
+    end
+end
