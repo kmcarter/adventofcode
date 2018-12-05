e9020085662149afbcce6811b30bfa4849014a5e
Kelly Carter <kelly@rainworksweb.com>
2018-12-04 23:21:48 -0800

    2018 day 5 part 2 completed!

--- 2018/day05/day05.rb
+++ 2018/day05/day05.rb
@@ -27,5 +27,18 @@ def react_polymer(str)
     return new_str
 end
 
-output = react_polymer input
-p output.length
+#output = react_polymer input
+#p output.length
+
+#part 2
+#input = 'dabAcCaCBAcCcaDA'
+shortest_length = 12000
+
+("a".."z").each do |to_delete|
+    new_str = String.new(input)
+    new_str.delete!(to_delete + to_delete.upcase)
+    reacted_str = react_polymer(new_str)
+    shortest_length = reacted_str.length if reacted_str.length < shortest_length
+end
+
+p shortest_length
