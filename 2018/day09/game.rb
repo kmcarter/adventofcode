a0c5f38483c22e482e50de643a8d8e947f4f5d92
Kelly Carter <kelly@rainworksweb.com>
2018-12-10 02:57:39 -0800

    Some refactoring. I must be doing something stupid.

--- 2018/day09/game.rb
+++ 2018/day09/game.rb
@@ -1,13 +1,13 @@
 class MarbleGame
     def initialize(num_players, last_marble)
         @last_marble = last_marble
-        @circle = []
+        @circle = [0]
         @players = []
         (0..num_players).each { |id| @players << Player.new(id+1) }
     end
 
     def play
-        (0..@last_marble).each do |i|
+        (1..@last_marble).each do |i|
             take_turn(i, @players[0])
             @players.rotate!(1)
         end
@@ -17,19 +17,18 @@ class MarbleGame
         @players
     end
 
-    def take_turn(marble_index, player)
-        
-        if marble_index != 0 && marble_index % 23 == 0
+    def take_turn(marble, player)
+        if marble % 23 == 0
             @circle.rotate!(-7)
             #p @circle.join(" ")
-            player.score(marble_index + @circle[0])
+            player.score(marble + @circle[0])
             @circle.slice!(0,1)
         elsif @circle.length > 0
             #p "#{@circle.join(" ")} -->"
             @circle.rotate!(2)
-            @circle.insert(0, marble_index)
+            @circle.insert(0, marble)
         else
-            @circle << marble_index
+            @circle << marble
         end
     end
 end
