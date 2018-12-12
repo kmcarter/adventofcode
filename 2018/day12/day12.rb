d9032ce9b245c24ea24da6d2cb4bbfca6b98b384
Kelly Carter <kelly@rainworksweb.com>
2018-12-12 10:05:08 -0800

    Completed 2018 day 12 part 1 - suck it Dave

--- 2018/day12/day12.rb
+++ 2018/day12/day12.rb
@@ -1,34 +1,49 @@
 #part 1
 
-input = File.open("example.txt").read.split("\n\n")
-initial_state = input[0].split("initial state: ")[1]
+input = File.open("input.txt").read.split("\n\n")
+initial_state = input[0].split("initial state: ")[1].chars.each_with_index.map { |plant,i| [ i, plant ] }
+p initial_state
 notes = input[1].split("\n")
-generations = [ [initial_state, 0] ]
+generations = [ initial_state ]
 
 notes.map! {|note| note.split(" => ") }
 notes.delete_if { |note| note[1] == "." }
 notes.map! {|note| note[0] }
 p notes
 
-(0..19).each do |gen_i|
-    next_gen = []
-    first_plant_idx = generations.last.index("#")
-    last_plant_idx = generations.last.rindex("#")
-    gen = "....." + generations.last[first_plant_idx..last_plant_idx] + "....."
-    p "gen #{gen_i} = #{gen}"
-
-    (2..gen.length-3).each do |i|
-        local_pots = gen[i-2..i+2]
-        produces_plant = notes.index(local_pots) != nil
-        p "[#{i}] #{local_pots} => #{produces_plant}"
-        
-        next_gen << (produces_plant ? "#" : ".")
-    end
-
-    generations << [ next_gen.join,  ]
-    #p "next #{gen_i+1} = #{next_gen.join}"
+def pad_plants(generation)
+    first_plant_idx = generation.index { |plant| plant[1] == "#" }
+    first_plant_id = generation[first_plant_idx][0]
+    last_plant_idx = generation.rindex { |plant| plant[1] == "#" }
+    last_plant_id = generation[last_plant_idx][0]
+    generation = generation[first_plant_idx..last_plant_idx]
+    (-4..-1).reverse_each {|i| generation.insert(0, [ first_plant_id+i, "." ]) }
+    (1..4).each {|i| generation.insert(generation.length, [ last_plant_id+i, "." ]) }
+    generation
 end
 
-generations.each_with_index { |gen, i| p "[#{i}] #{gen}" }
+(0..19).each do |gen_i|
+    next_gen = []
+    gen = pad_plants(generations.last)
+    #p "gen #{gen_i} = #{gen}"
 
-p generations.last.count("#")
+    (2..gen.length-3).each do |i|
+        local_pots = gen[i-2..i+2].map {|x,y| y}.join
+        produces_plant = notes.index(local_pots) != nil
+        #p "[#{i}] #{local_pots} => #{produces_plant}"
+        
+        next_gen << [ gen[i][0], (produces_plant ? "#" : ".") ]
+    end
+    
+    generations << next_gen
+    #p "next #{gen_i+1} = #{next_gen.join}"
+end
+p generations.last
+
+generations.each_with_index { |gen, i| p "[#{i}] #{gen.map{ |x,y| y }.join}" }
+
+sum = 0
+generations.last.each do |i, plant|
+    sum += i if plant == "#"
+end
+p sum
