25fbda756671f49f240a21420d72ce4f6cf8f0dc
Kelly Carter <kelly@rainworksweb.com>
2018-12-18 16:18:11 -0800

    2018 day 17 part 1 - parsing and building of initial map

--- 2018/day17/day17.rb
+++ 2018/day17/day17.rb
@@ -1,16 +1,14 @@
 spring = [500, 0]
-map = {}
 input = File.open("./example.txt").read.split("\n")
 input.map! {|line| line.split(", ")}
-p input
 input.map! do |coords|
     x = nil
     y = nil
     coords.each do |coord|
-        coord_split = coords[0].split("=")
+        coord_split = coord.split("=")
         if coord_split[1].index("..") != nil
-            coord_split[1] = coord_split[1].split("..").each {|pole| pole.to_i }
-            p coord_split[1]
+            coord_split[1] = coord_split[1].split("..")
+            coord_split[1].map! {|pole| pole.to_i }
         else
             coord_split[1] = coord_split[1].to_i
         end
@@ -22,12 +20,43 @@ input.map! do |coords|
 end
 p input
 
-input.each do |coords|
-    if coords[0][1].index("..") != nil
-        
-    elsif coords[1][1].index("..") != nil
+smallest_x = 500
+largest_x = 500
+largest_y = 0
 
+input.each do |coords|
+    if coords[0].kind_of? Integer
+        smallest_x = coords[0] if coords[0] < smallest_x
+        largest_x = coords[0] if coords[0] > largest_x
     else
-        
+        smallest_x = coords[0].min if coords[0].min < smallest_x
+        largest_x = coords[0].max if coords[0].max > largest_x
+    end
+
+    if coords[1].kind_of? Integer
+        largest_y = coords[1] if coords[1] > largest_y
+    else
+        largest_y = coords[1].max if coords[1].max > largest_y
     end
 end
+
+p "Smallest to largest X: #{smallest_x} -> #{largest_x}"
+p "Largest Y: #{largest_y}"
+
+map = []
+largest_y.succ.times.each do
+    map << Array.new(largest_x - smallest_x, ".")
+end
+#map.each {|row| p row.join }
+
+input.each do |coords|
+    if coords[0].kind_of?(Integer) && coords[1].kind_of?(Integer)
+        map[coords[1]][coords[0]-495] = "#"
+    elsif coords[1].kind_of? Integer
+        coords[0][0].upto(coords[0][1]).each { |x| map[coords[1]][x-495] = "#" }
+    else
+        coords[1][0].upto(coords[1][1]).each { |y| map[y][coords[0]-495] = "#" }
+    end
+end
+map[spring[1]][spring[0]-495] = "+"
+map.each {|row| p row.join }
