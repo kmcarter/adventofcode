37eed41d891d3908f49f8f731b3dc6864ccbdc2e
Kelly Carter <kelly@rainworksweb.com>
2018-12-23 12:02:16 -0800

    Far enough with day 19 to find the inner loop

--- 2018/day19/calculator.rb
+++ 2018/day19/calculator.rb
@@ -4,16 +4,32 @@ class Calculator
         @register = initial_register_vals
     end
 
-    def run(ip, instructions)
-        loop do
-            if instructions[ip] == nil
+    def run(ip_register, instructions)
+        iteration_count = 0
+        ip = 0
+        last_reg1_val = 0
+        #opscode_counts_prev = {}
+        #opscode_counts = Hash.new(0)
+        7200.times.each do
+            @register[ip_register] = ip
+            register_before = @register
+
+            instruction = instructions[ip]
+            #puts "Instruction pointer is #{ip}"
+
+            if instruction == nil
                 break
             end
-            register_before = @register
-            @register[0] = ip
-            @register = public_send(instructions[ip][0], @register, instructions[ip])
-            puts "ip=#{ip} #{register_before} #{instructions[ip]} #{@register}}"
-            ip = @register[0] + 1
+
+            @register = public_send(instruction[0], @register, instruction)
+            #opscode_counts[instructions[@register[ip]][0]] += 1
+            puts "ip=#{ip} #{register_before} #{instruction} #{@register}} ##{iteration_count}"
+            iteration_count += 1
+            ip = @register[ip_register] + 1
+
+            #puts "Register 1 val has reset after #{iteration_count} iterations! register = #{@register}" if @register[1] < last_reg1_val
+            last_reg1_val = @register[1]
+            #puts "ip set to #{ip}"
         end
         @register
     end
@@ -81,19 +97,19 @@ class Calculator
 
     def gtir(registers, inst)
         result = Marshal.load(Marshal.dump(registers))
-        result[inst[3]] = inst[1] <=> registers[inst[2]]
+        result[inst[3]] = inst[1] > registers[inst[2]] ? 1 : 0
         result
     end
 
     def gtri(registers, inst)
         result = Marshal.load(Marshal.dump(registers))
-        result[inst[3]] = registers[inst[1]] <=> inst[2]
+        result[inst[3]] = registers[inst[1]] > inst[2] ? 1 : 0
         result
     end
 
     def gtrr(registers, inst)
         result = Marshal.load(Marshal.dump(registers))
-        result[inst[3]] = registers[inst[1]] <=> registers[inst[2]]
+        result[inst[3]] = registers[inst[1]] > registers[inst[2]] ? 1 : 0
         result
     end
 
--- 2018/day19/day19.rb
+++ 2018/day19/day19.rb
@@ -1,6 +1,7 @@
 require "./calculator.rb"
+p "Started at #{Time.now}"
 
-input = File.open("./example.txt").read.split("\n")
+input = File.open("./input.txt").read.split("\n")
 input.map! do |row|
     row.split(" ").each_with_index.map do |inst, i|
         if i > 0
@@ -16,4 +17,4 @@ ip = input[0][1]
 input.delete_at(0)
 calc = Calculator.new([0,0,0,0,0,0])
 
-p calc.run(ip, input)
+calc.run(ip, input)
