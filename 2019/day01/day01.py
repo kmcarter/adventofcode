a6c495fd743024870c155fbed2453da82bf14aca
Kelly Carter <kmcarter@uw.edu>
2019-12-04 13:58:03 -0800

    Day 1 part 2 complete

--- 2019/day01/day01.py
+++ 2019/day01/day01.py
@@ -4,7 +4,14 @@ input = Path('./2019/day01/input.txt')
 input_masses = [int(line) for line in input.open()]
 total_mass = 0
 
+def calc_fuel(mass):
+    fuel = mass // 3 - 2
+    return 0 if fuel < 0 else fuel
+
 for mass in input_masses:
-    total_mass += mass // 3 - 2
+    fuel = calc_fuel(mass)
+    while fuel > 0:
+        total_mass += fuel
+        fuel = calc_fuel(fuel)
 
 print(total_mass)
